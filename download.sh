fcafd8c050d971ac3f2fd78eef0913d703b75724
Christos Gkantidis <christosg8@gmail.com>
2023-09-13 11:38:52 +0200

    Add argparser module and an option to print the sizes in human-readable format

--- download.sh
+++ download.sh
@@ -1,1 +1,2 @@
 git clone --depth 1 https://github.com/bshoshany/thread-pool.git
+git clone --depth 1 https://github.com/p-ranav/argparse.git
--- mt_du.cpp
+++ mt_du.cpp
@@ -1,5 +1,8 @@
-#include "thread-pool/include/BS_thread_pool.hpp"
+#include <BS_thread_pool.hpp>
+#include <argparse/argparse.hpp>
+
 #include <atomic>
+#include <cmath>
 #include <filesystem>
 #include <forward_list>
 #include <future>
@@ -12,11 +15,27 @@
 namespace fs = std::filesystem;
 static std::error_code const no_error;
 
-void
-get_rec_dir_size(fs::path const &path,
-                 std::atomic<std::uintmax_t> &total_size,
-                 BS::thread_pool &pool) {
+void get_rec_dir_size(
+    fs::path const &path,
+    std::atomic<std::uintmax_t> &total_size,
+    BS::thread_pool &pool) {
   std::error_code ec;
+
+  {
+    auto const status = fs::symlink_status(path);
+    if (status.type() != fs::file_type::directory) {
+      if (status.type() == fs::file_type::regular) {
+        std::uintmax_t const file_size{fs::file_size(path, ec)};
+        if (ec != no_error) {
+          std::cerr << ec.message() << " (" << path << ")\n";
+        } else {
+          total_size += file_size;
+        }
+      }
+      return;
+    }
+  }
+
   for (auto dir_it = fs::directory_iterator(path, ec);
        dir_it != fs::directory_iterator();
        ++dir_it) {
@@ -44,77 +63,118 @@ get_rec_dir_size(fs::path const &path,
       break;
     }
     case fs::file_type::directory: {
-      pool.push_task(get_rec_dir_size,
-                     dir_it->path(),
-                     std::ref(total_size),
-                     std::ref(pool));
+      pool.push_task(
+          get_rec_dir_size,
+          dir_it->path(),
+          std::ref(total_size),
+          std::ref(pool));
       break;
     }
     case fs::file_type::symlink:
+    case fs::file_type::none:
+    case fs::file_type::not_found:
+    case fs::file_type::block:
+    case fs::file_type::character:
+    case fs::file_type::fifo:
+    case fs::file_type::socket:
+    case fs::file_type::unknown:
       break;
-    case fs::file_type::none: {
-      std::cerr << dir_it->path() << " has `not-evaluated-yet` type\n";
-      break;
-    }
-    case fs::file_type::not_found: {
-      std::cerr << dir_it->path() << " does not exist\n";
-      break;
-    }
-    case fs::file_type::block: {
-      std::cerr << dir_it->path() << " is a block device\n";
-      break;
-    }
-    case fs::file_type::character: {
-      std::cerr << dir_it->path() << " is a character device\n";
-      break;
-    }
-    case fs::file_type::fifo: {
-      std::cerr << dir_it->path() << " is a named IPC pipe\n";
-      break;
-    }
-    case fs::file_type::socket: {
-      std::cerr << dir_it->path() << " is a named IPC socket\n";
-      break;
-    }
-    case fs::file_type::unknown: {
-      std::cerr << dir_it->path() << " has `unknown` type\n";
-      break;
-    }
     }
   }
 }
 
-std::vector<fs::path>
-get_paths(std::span<std::string_view const> args) {
+class HumanReadable {
+public:
+  std::uintmax_t m_size{};
+
+private:
+  friend std::ostream &operator<<(std::ostream &os, HumanReadable const &hr) {
+    int o{};
+    auto mantissa = static_cast<double>(hr.m_size);
+    while (mantissa >= 1024.0) {
+      mantissa /= 1024.0;
+      ++o;
+    }
+    os << std::ceil(mantissa * 10.0) / 10.0 << "BKMGTPE"[o];
+    return o != 0 ? (os << "B") : os;
+  }
+};
+
+std::vector<fs::path> get_paths(std::vector<std::string> const &paths_str) {
   std::vector<fs::path> paths;
-  paths.reserve(args.size() - 1);
-  for (auto const &arg : std::ranges::drop_view{args, 1}) {
-    paths.emplace_back(arg);
+  paths.reserve(paths_str.size());
+  for (auto const &path_str : paths_str) {
+    paths.emplace_back(path_str);
   }
   return paths;
 }
 
-int
-main(int argc, char const *const *argv) {
-  std::vector<std::string_view> args(
-      argv,
-      std::next(argv, static_cast<std::ptrdiff_t>(argc)));
+bool is_printable(std::string const &s) {
+  return std::all_of(s.begin(), s.end(), [](char ch) {
+    return std::isprint(ch);
+  });
+}
 
-  std::vector<fs::path> const paths = get_paths(args);
+argparse::ArgumentParser construct_argument_paraser() {
+  argparse::ArgumentParser program("mt_du");
+  program.add_argument("-H", "--human-readable")
+      .help("display the sizes in a human-readable format")
+      .default_value(false)
+      .implicit_value(true);
+  program.add_argument("paths")
+      .help("the list of paths for which to print their disk size")
+      .nargs(argparse::nargs_pattern::at_least_one);
+
+  return program;
+}
+
+int main(int argc, char const *const *argv) {
+  auto program = construct_argument_paraser();
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (std::runtime_error const &err) {
+    std::cerr << err.what() << '\n';
+    std::cerr << program;
+    std::exit(EXIT_FAILURE);
+  }
+
+  auto const hr = program.get<bool>("--human-readable");
+  auto const paths_str = program.get<std::vector<std::string>>("paths");
+  auto const paths = get_paths(paths_str);
   std::vector<std::atomic<std::uintmax_t>> sizes(paths.size());
 
   BS::thread_pool pool;
   for (std::size_t i = 0; i < paths.size(); ++i) {
-    pool.push_task(get_rec_dir_size,
-                   std::cref(paths[i]),
-                   std::ref(sizes[i]),
-                   std::ref(pool));
+    pool.push_task(
+        get_rec_dir_size,
+        std::cref(paths[i]),
+        std::ref(sizes[i]),
+        std::ref(pool));
   }
 
   pool.wait_for_tasks();
 
   for (std::size_t i = 0; i < paths.size(); ++i) {
-    std::cout << paths[i].c_str() << ' ' << sizes[i] << '\n';
+    auto path = paths[i].string();
+    if (is_printable(path)) {
+      std::cout << path << ' ';
+      if (hr) {
+        std::cout << HumanReadable(sizes[i]) << '\n';
+      } else {
+        std::cout << sizes[i] << '\n';
+      }
+    } else {
+      std::cerr << "ERROR: Path contains unprintable characters:\n";
+      for (char ch : path) {
+        if (!std::isalnum(ch) && ch != '_' && ch != '-' && ch != '/') {
+          fprintf(stderr, "$'\\%03o'", ch);
+        } else {
+          fprintf(stderr, "%c", ch);
+        }
+      }
+      std::cerr << '\n';
+    }
   }
 
   return 0;
