488120f9bac1766f5397ac2520d197d0236711c8
Christos Gkantidis <normalmatter@proton.me>
2024-01-11 13:23:08 +0100

    Add script to build mt_du with gcc/clang in Release/Debug configurations, and run the static analysis tools

---
+++ build_all.sh
@@ -0,0 +1,14 @@
+cmake -S . -B build-release-gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DENABLE_STATIC_CHECKS=ON
+cmake --build build-release-gcc -j
+cmake --install build-release-gcc --prefix .
+
+cmake -S . -B build-debug-gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DENABLE_STATIC_CHECKS=ON
+cmake --build build-debug-gcc -j
+cmake --install
+
+cmake -S . -B build-release-clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DENABLE_STATIC_CHECKS=ON
+cmake --build build-release-clang -j
+
+cmake -S . -B build-debug-clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DENABLE_STATIC_CHECKS=ON
+cmake --build build-debug-clang -j
+
